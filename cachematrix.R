b966acad85f77300b17396bbb5d8683b25546d7b
ginahvitus <robert.sheynin@stonybrook.edu>
2015-11-20 19:12:39 -0500

    Added comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of this of function is to generate
+## and solve special matricies and their inverses
+## using the two function makeCacheMatrix which
+## will provide methods for getting/setting matrix
+## and inverse values
 
-## Write a short comment describing this function
+## makeCacheMatrix will provide the value
+## entries for the cacheSolve function
+## specifically determining the get/set
+## functions for the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function solves for the inverse of 
+## the cachedMatrix, the product of the
+## makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
